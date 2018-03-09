1572f320d2f8a42505a0a112893b7e141ca4024e
benjamin malbrel <benjaminmalbrel@benjamins-MacBook-Pro.local>
2018-03-09 19:26:21 +0200

    Working connection register page WIP

---
+++ SportsFun/SportsFun/Controllers/Inscription.swift
@@ -0,0 +1,21 @@
+//
+//  Inscription.swift
+//  SportsFun
+//
+//  Created by benjamin malbrel on 09/03/2018.
+//  Copyright © 2018 benjamin malbrel. All rights reserved.
+//
+
+import UIKit
+
+class Inscription : UIViewController {
+    
+    @IBOutlet var tfUserName : UITextField!
+    @IBOutlet var tfEmail : UITextField!
+    @IBOutlet var tfFirstName : UITextField!
+    @IBOutlet var tfLastName : UITextField!
+    @IBOutlet var tfPassword : UITextField!
+    @IBOutlet var dpBirthdate : UIDatePicker!
+    
+    
+}
